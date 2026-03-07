655103bf2d65257589fe0d38ab93562818205a9d
정승혜 <seunghaejung@jeongseunghyeui-MacBookPro.local>
2026-03-07 14:17:41 +0900

    편지 최종본5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,18 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "미래로 보내는 편지",
-  description: "1년 뒤의 나에게 보내는 편지 서비스",
+  description: "1년 뒤의 나에게 소중한 마음을 전하세요.",
+  openGraph: {
+    title: "미래로 보내는 편지",
+    description: "1년 뒤의 나에게 소중한 마음을 전하세요.",
+    images: [
+      {
+        url: "/shareimage.png", // public 폴더에 넣은 파일 이름
+        width: 700,
+        height: 590,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
